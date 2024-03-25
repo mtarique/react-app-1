bba20d012c8a7d7a5d63ad81dafefb1c9fb7b447
mtarique <mtarique@outlook.com>
2024-03-25 23:38:39 +0530

    Added some more logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,10 @@ export default function TextForm() {
         setText(text.toUpperCase())
     }
 
+    const handleLoClick = () => {
+        setText(text.toLowerCase())
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
@@ -17,15 +21,25 @@ export default function TextForm() {
     }
 
     return (
-        <div>
-            <div className="mb-3">
-                <label htmlFor="myBox" className="form-label h3">Enter the text to analyze below</label>
-                <textarea className="form-control" id="myBox" rows="5" value={text} onChange={handleOnChange}></textarea>
+        <>
+            <div className="container">
+                <div className="mb-3">
+                    <label htmlFor="myBox" className="form-label h3">Enter the text to analyze below</label>
+                    <textarea className="form-control" id="myBox" rows="5" value={text} onChange={handleOnChange}></textarea>
+                </div>
+                <div className="mb-3 d-flex justify-content-start">
+                    <button className="btn btn-primary me-2" onClick={handleUpClick}>Convert to Uppercase</button> 
+                    <button className="btn btn-primary me-2" onClick={handleLoClick}>Convert to Lowercase</button> 
+                    <button className="btn btn-secondary me-2" onClick={handleClClick}>Clear</button>
+                </div>
             </div>
-            <div className="mb-3 d-flex justify-content-end">
-                <button className="btn btn-secondary me-2" onClick={handleClClick}>Clear</button>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button> 
+            <div className="container">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>Reading time {0.008 * text.split(" ").length} minutes</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
             </div>
-        </div>
+        </>
     )
 }
